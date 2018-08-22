e819d90728b550ed9faa792ba954571652471cf1
Vadim Sidorik <vadim.sidorik@gismart.com>
2018-08-22 19:45:20 +0300

    Add base di implementation

--- core/src/com/yarrtest/balloon/screens/GameScreen.kt
+++ core/src/com/yarrtest/balloon/screens/GameScreen.kt
@@ -1,16 +1,23 @@
 package com.yarrtest.balloon.screens
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.utils.viewport.FitViewport
 import com.yarrtest.balloon.BalloonGame
 import com.yarrtest.balloon.base.AppConfig
 import com.yarrtest.balloon.base.BaseScreen
+import com.yarrtest.balloon.screens.di.ScreenScope
 import com.yarrtest.balloon.screens.game.GameController
+import com.yarrtest.balloon.screens.game.di.GameScreenComponent
+import com.yarrtest.balloon.screens.game.views.Balloon
+import dagger.Module
+import dagger.Provides
 
 /**
  * Created by etb on 21.08.2018.
  */
+@Module
 class GameScreen(
         game: BalloonGame,
         config: AppConfig
@@ -19,17 +26,18 @@ class GameScreen(
         FitViewport(config.camWidth, config.camHeight)
 ) {
 
-    private var background: Image? = null
+    var component: GameScreenComponent? = null
+    private set
+
     private var isShown = false
     private var controller: GameController? = null
 
     init {
-
+        component = game.component!!.plus(this)
         controller?.let {
             lifecycleListener = it
             addInputListener(it)
         }
-
     }
 
     override fun prepareStage(stage: Stage) {
@@ -41,5 +49,12 @@ class GameScreen(
     override fun render(delta: Float) {
         super.render(delta)
         controller?.update(delta)
+    }
+
+    @ScreenScope
+    @Provides
+    fun createBalloonView(): Balloon {
+        val texture = Texture(Gdx.files.internal("ball.png"))
+        return Balloon(stage!!, texture)
     }
 }
